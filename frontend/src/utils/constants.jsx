6bd98e56eaef1f3e780cfad08b2b43566966bcf7
Salinporn R. <110949709+Salinporn@users.noreply.github.com>
2024-09-17 22:46:39 +0700

    Update constants.jsx

--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,2 +1,6 @@
-export const HOST = "172.20.10.11";
+/* Defining the host and backend port explicitly for deployment on IIS. */
+/* This ensures the frontend can communicate with the backend, even if */
+/* the IIS configuration doesn't properly forward relative URLs. */
+
+export const HOST = "xxx.xx.xx.xx"; /*IP address*/
 export const BACKEND_PORT = "3001";
